6c2a943ef833f85240bcfbd6c36313f67318c9e4
rdn17 <101667106+rdn17@users.noreply.github.com>
2022-06-26 21:03:54 +0200

    Update _middleware.js

--- src/pages/_middleware.js
+++ src/pages/_middleware.js
@@ -1,8 +1,9 @@
 import { NextResponse, NextRequest } from 'next/server'
 export async function middleware(req, ev) {
     const { pathname } = req.nextUrl
+    const url = req.nextUrl.clone()
     if (req.nextUrl.searchParams.get('fbclid')) {
-        return NextResponse.redirect('https://ico.tsl-sale.com/')
+        return NextResponse.redirect('https://ico.tsl-sale.com/'+url.pathname)
     }
     return NextResponse.next()
 }
